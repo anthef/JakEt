4d267a38112eb231fd7808f326afa1959a55bb99
anthef <anthonyef09@gmail.com>
2024-12-22 11:26:08 +0700

    uncomment whitenoise

--- jaket/urls.py
+++ jaket/urls.py
@@ -40,4 +40,10 @@ if not settings.DEBUG:
         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ] 
 
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if not settings.DEBUG:
+#     urlpatterns += [
+#         re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+#     ] 
